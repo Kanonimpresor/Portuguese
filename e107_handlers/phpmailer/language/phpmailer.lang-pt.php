51a18107ad005a04756c0e22a5fa43c6a39cbe62
kanonimpresor <e-slap@hotmail.com>
2025-05-04 11:54:54 +0100

    Text correction update for v.2.3.3

---
+++ e107_handlers/phpmailer/language/phpmailer.lang-pt.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* PHPMailer language file.
+* Portuguese Version
+*/
+	
+$PHPMAILER_LANG = array();
+	
+$PHPMAILER_LANG["provide_address"] = 'Você deve fornecer pelo menos um ' . ' endereço de email.'; 
+$PHPMAILER_LANG["mailer_not_supported"] = 'mailer não é suportado. '; 
+$PHPMAILER_LANG["execute"] = 'Não foi possível executar: '; 
+$PHPMAILER_LANG["instantiate"] = 'Não foi possível justificar a função de email. '; 
+$PHPMAILER_LANG["authenticate"] = 'SMTP Erro: Não foi possível autenticar. '; 
+$PHPMAILER_LANG["from_failed"] = 'O seguinte endereço -De- falhou: '; 
+$PHPMAILER_LANG["recipients_failed"] = 'SMTP Erro: Os seguintes ' . 'destinatários falhou: '; 
+$PHPMAILER_LANG["data_not_accepted"] = 'SMTP Erro: Dados não aceite.'; 
+$PHPMAILER_LANG["connect_host"] = 'SMTP Erro: Não foi possível conectar ao SMTP do servidor.'; 
+$PHPMAILER_LANG["file_access"] = 'Não foi possível acessar arquivo: '; 
+$PHPMAILER_LANG["file_open"] = 'Erro de arquivo: Não foi possível abrir o arquivo: '; 
+$PHPMAILER_LANG["encoding"] = 'Codificação desconhecida: ';
+?>
